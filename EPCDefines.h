07a8af52969ca557558c751f9b08c6043027794c
Everton Cunha <ever.cunha@gmail.com>
2012-08-10 16:40:49 -0300

    comments

--- EPCDefines.h
+++ EPCDefines.h
@@ -5,23 +5,27 @@
 //
 
 
-// helpers
-
+/*
+ Misc helpers.
+ */
 #define $sf(obj, ...) (NSString*)[NSString stringWithFormat:obj, __VA_ARGS__]
 
 
-// degree and radians
-
+/*
+ Andle helpers.
+ */
 #define degreesToRadians(x) (M_PI * (x) / 180.0)
 #define radiansToDegrees(x) (180.0*(x)/ M_PI)
 
 
-// color
-
+/*
+ UIColor helpers.
+ */
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 #define UIColorFromRGB_A(rgbValue, a) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:a]
 
 
-// system version
-
-#define SYSTEM_VERSION_LESS_THAN(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+/*
+ Check iOS Version.
+ */
+#define IOS_VERSION_LESS_THAN(string) ([[[UIDevice currentDevice] systemVersion] compare:string options:NSNumericSearch] == NSOrderedAscending)
