7e4060cc5107c0c6cd4c225fe03adab282d81067
Everton Cunha <ever.cunha@gmail.com>
2012-08-14 12:30:22 -0300

    fix for ShareKit Issue #321

---
+++ Others/SHKConfiguration+ShareKitFix.h
@@ -0,0 +1,13 @@
+//
+//  SHKConfiguration+ShareKitFix.h
+//  D3Project
+//
+//  Created by Everton Cunha on 14/08/12.
+//  Copyright (c) 2012 Ring. All rights reserved.
+//
+
+#import "SHKConfiguration.h"
+
+@interface SHKConfiguration (ShareKitFix)
+
+@end
---
+++ Others/SHKConfiguration+ShareKitFix.m
@@ -0,0 +1,17 @@
+//
+//  SHKConfiguration+ShareKitFix.m
+//  D3Project
+//
+//  Created by Everton Cunha on 14/08/12.
+//  Copyright (c) 2012 Ring. All rights reserved.
+//
+
+#import "SHKConfiguration+ShareKitFix.h"
+
+@implementation SHKConfiguration (ShareKitFix)
+
++ (id)allocWithZone:(NSZone *)zone {
+    return [super allocWithZone:zone];
+}
+
+@end
