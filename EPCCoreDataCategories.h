ee1073e3bbc596bd7dbdd978f8bdc97fc48c5d15
Everton Cunha <ever.cunha@gmail.com>
2012-08-14 12:03:59 -0300

    category for Core Data. first commit.

--- EPCCoreDataCategories.h
+++ EPCCoreDataCategories.h
@@ -1,13 +1,12 @@
 //
-//  NSManagedObjectContext+EPCCoreDataCategories.h
-//  Renner
+//  EPCCoreDataCategories.h
 //
 //  Created by Everton Cunha on 13/08/12.
-//  Copyright (c) 2012 Ring. All rights reserved.
 //
 
 #import <CoreData/CoreData.h>
 
 @interface NSManagedObjectContext (EPCCoreDataCategories)
-
+- (NSSet *)fetchObjectsForEntityName:(NSString *)newEntityName
+					   withPredicate:(id)stringOrPredicate, ...;
 @end
