3851a3e4cde1ae1c25fc5912d3e680bc2cf8d792
Everton Cunha <ever.cunha@gmail.com>
2012-08-14 12:32:23 -0300

    fix for ShareKit Issue #321

--- Others/SHKConfiguration+ShareKitFix.h
+++ Others/SHKConfiguration+ShareKitFix.h
@@ -1,9 +1,7 @@
 //
 //  SHKConfiguration+ShareKitFix.h
-//  D3Project
 //
 //  Created by Everton Cunha on 14/08/12.
-//  Copyright (c) 2012 Ring. All rights reserved.
 //
 
 #import "SHKConfiguration.h"
--- Others/SHKConfiguration+ShareKitFix.m
+++ Others/SHKConfiguration+ShareKitFix.m
@@ -1,9 +1,7 @@
 //
 //  SHKConfiguration+ShareKitFix.m
-//  D3Project
 //
 //  Created by Everton Cunha on 14/08/12.
-//  Copyright (c) 2012 Ring. All rights reserved.
 //
 
 #import "SHKConfiguration+ShareKitFix.h"
