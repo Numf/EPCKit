96888813334cad9c04011578f8cc3b852ee90795
Everton Cunha <ever.cunha@gmail.com>
2012-08-14 12:30:48 -0300

    fix to use ShareKit reachability file with ASIHTTPRequest

---
+++ Others/Reachability+ASI+ShareKitFix.h
@@ -0,0 +1,13 @@
+//
+//  Reachability+ASI+ShareKitFix.h
+//
+//  Created by Everton Cunha on 14/08/12.
+//
+
+/*
+ This resolves the error when using ShareKit Reachability instead the one from ASIHTTPRequest
+ */
+
+extern NSString *const kInternetConnection;
+extern NSString *const kLocalWiFiConnection;
+extern NSString *const kReachabilityChangedNotification;
---
+++ Others/Reachability+ASI+ShareKitFix.m
@@ -0,0 +1,11 @@
+//
+//  ASIHTTPRequest+ReachFix.m
+//
+//  Created by Everton Cunha on 14/08/12.
+//
+
+#import "Reachability+ASI+ShareKitFix.h"
+
+NSString *const kInternetConnection  = @"InternetConnection";
+NSString *const kLocalWiFiConnection = @"LocalWiFiConnection";
+NSString *const kReachabilityChangedNotification = @"NetworkReachabilityChangedNotification";
