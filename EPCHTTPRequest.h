21fd1a40f49c165c14160dc3c81816d76a8beec5
Everton Cunha <ever.cunha@gmail.com>
2012-08-17 11:53:01 -0300

    HTTP Request class. first commit.
    Got tired of ASIHTTPRequest crashes, so I made mine.

--- EPCHTTPRequest.h
+++ EPCHTTPRequest.h
@@ -1,13 +1,84 @@
 //
 //  EPCHTTPRequest.h
-//  Renner
 //
 //  Created by Everton Cunha on 17/08/12.
-//  Copyright (c) 2012 Ring. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface EPCHTTPRequest : NSObject
+@class EPCHTTPRequest;
 
+@protocol EPCHTTPRequestDelegate <NSObject>
+@optional
+/*
+ Fired when the request succeeds.
+ */
+- (void)epcHTTPRequestFinished:(EPCHTTPRequest*)request;
+
+/*
+ Fired when the request fails.
+ */
+- (void)epcHTTPRequestFailed:(EPCHTTPRequest *)request;
+
+/*
+ Fired just before the request starts
+ */
+- (void)epcHTTPRequestStarted:(EPCHTTPRequest *)request;
+@end
+
+@interface EPCHTTPRequest : NSOperation <EPCHTTPRequestDelegate>
+/*
+ Allocates the request.
+ */
+- (EPCHTTPRequest*)initWithURL:(NSURL *)url delegate:(id<EPCHTTPRequestDelegate>)delegate;
+
+/*
+ Autoreleased request.
+ */
++ (EPCHTTPRequest*)requestWithURL:(NSURL*)url delegate:(id<EPCHTTPRequestDelegate>)delegate;
+
+/*
+ Autoreleased request that automatically starts.
+ */
++ (EPCHTTPRequest*)startRequestWithURL:(NSURL *)url delegate:(id<EPCHTTPRequestDelegate>)delegate;
+
+/*
+ Clear the delegate and cancel the request.
+ */
+- (void)clearDelegatesAndCancel;
+
+/*
+ Starts the request Asynchronously.
+ */
+- (void)startAsynchronous;
+
+/*
+ Starts the request Synchronously.
+ */
+- (void)startSynchronous;
+
+/*
+ The delegate.
+ */
+@property (assign) id<EPCHTTPRequestDelegate> delegate;
+
+/*
+ Request error when it fails.
+ */
+@property (readonly) NSError *error;
+
+/*
+ The data returned from the request.
+ */
+@property (readonly) NSData *responseData;
+
+/*
+ The data as string.
+ */
+@property (readonly) NSString *responseString;
+
+/*
+ The request URL.
+ */
+@property (retain) NSURL *url;
 @end
--- EPCHTTPRequest.m
+++ EPCHTTPRequest.m
@@ -1,13 +1,120 @@
 //
 //  EPCHTTPRequest.m
-//  Renner
 //
 //  Created by Everton Cunha on 17/08/12.
-//  Copyright (c) 2012 Ring. All rights reserved.
 //
 
 #import "EPCHTTPRequest.h"
 
-@implementation EPCHTTPRequest
+@interface EPCHTTPRequest() {
+	NSOperationQueue *_operationQueue;
+	NSError *_error;
+	NSString *_responseString;
+}
+@end
 
+@implementation EPCHTTPRequest
++ (EPCHTTPRequest*)requestWithURL:(NSURL *)url delegate:(id<EPCHTTPRequestDelegate>)delegate {
+	EPCHTTPRequest *me = [[[self new] initWithURL:url delegate:delegate] autorelease];
+	return me;
+}
++ (EPCHTTPRequest*)startRequestWithURL:(NSURL *)url delegate:(id<EPCHTTPRequestDelegate>)delegate {
+	EPCHTTPRequest *me = [self requestWithURL:url delegate:delegate];
+	[me startAsynchronous];
+	return me;
+}
+- (id)initWithURL:(NSURL *)url delegate:(id<EPCHTTPRequestDelegate>)delegate {
+	self = [[self class] new];
+	if (self) {
+		self.url = url;
+		self.delegate = delegate;
+	}
+	return self;
+}
+- (void)dealloc
+{
+    [_operationQueue cancelAllOperations];
+	[_operationQueue release];
+	_operationQueue = nil;
+	self.url = nil;
+	self.delegate = nil;
+	[_responseData release];
+    [super dealloc];
+}
+- (void)clearDelegatesAndCancel {
+	self.delegate = nil;
+	[_operationQueue cancelAllOperations];
+}
+- (void)startAsynchronous {
+	NSAssert(_url, @"%@ %s no URL to start the request.", NSStringFromClass([self class]), __PRETTY_FUNCTION__);
+	
+	if (_url) {
+		[_operationQueue cancelAllOperations];
+		if (_operationQueue)
+			_operationQueue = [NSOperationQueue new];
+		
+		EPCHTTPRequest *operation = [[EPCHTTPRequest new] autorelease];
+		operation.url = _url;
+		operation.delegate = (id<EPCHTTPRequestDelegate>)self;
+		[_operationQueue addOperation:operation];
+	}
+}
+- (void)epcHTTPRequestStarted:(EPCHTTPRequest*)request {
+	if ([_delegate respondsToSelector:@selector(epcHTTPRequestStarted:)])
+		[_delegate epcHTTPRequestStarted:self];
+}
+- (void)epcHTTPRequestFinished:(EPCHTTPRequest*)request {
+	[_responseString release];
+	_responseString = nil;
+	[_responseData release];
+	_responseData = [request.responseData retain];
+	if ([_delegate respondsToSelector:@selector(epcHTTPRequestFinished:)])
+		[_delegate epcHTTPRequestFinished:self];
+}
+- (void)epcHTTPRequestFailed:(EPCHTTPRequest*)request {
+	[_responseString release];
+	_responseString = nil;
+	[_error release];
+	_error = [request.error retain];
+	if ([_delegate respondsToSelector:@selector(epcHTTPRequestFailed:)])
+		[_delegate epcHTTPRequestFailed:self];
+}
+- (NSString *)responseString {
+	if (!_responseString && _responseData) {
+		_responseString = [[NSString alloc] initWithData:_responseData encoding:NSUTF8StringEncoding];
+	}
+	return _responseString;
+}
+- (void)startSynchronous {
+	BOOL isDone = NO;
+	if (_url) {
+		while (![self isCancelled] && !isDone) {
+			if ([_delegate respondsToSelector:@selector(epcHTTPRequestStarted:)])
+				[(EPCHTTPRequest*)_delegate performSelectorOnMainThread:@selector(epcHTTPRequestStarted:) withObject:self waitUntilDone:YES];
+			if ([self isCancelled])
+				break;
+			_responseData = [[NSData alloc] initWithContentsOfURL:_url options:NSDataReadingUncached error:&_error];
+			if ([self isCancelled])
+				break;
+			if (_responseData && !_error) {
+				if ([_delegate respondsToSelector:@selector(epcHTTPRequestFinished:)])
+					[(EPCHTTPRequest*)_delegate performSelectorOnMainThread:@selector(epcHTTPRequestFinished:) withObject:self waitUntilDone:YES];
+			}
+			else {
+				if ([_delegate respondsToSelector:@selector(epcHTTPRequestFailed:)])
+					[(EPCHTTPRequest*)_delegate performSelectorOnMainThread:@selector(epcHTTPRequestFailed:) withObject:self waitUntilDone:YES];
+			}
+			isDone = YES;
+		}
+	}
+}
+- (void)main {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	[self startSynchronous];
+	[pool drain];
+}
+- (void)cancel {
+	self.delegate = nil;
+	[super cancel];
+}
 @end
