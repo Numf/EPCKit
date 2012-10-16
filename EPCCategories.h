c45f8d0725a5ca030e2349bbf17938d5b746db69
Everton Cunha <ever.cunha@gmail.com>
2012-10-16 11:28:31 -0300

    new categories

--- EPCCategories.h
+++ EPCCategories.h
@@ -17,7 +17,9 @@
 
 @interface UIView (EPCCategories)
 + (id)loadFromNib;
-- (UIImage*)renderToImage;
+- (UIImage*)renderToImageOfSize:(CGSize)size opaque:(BOOL)opaque;
+- (void)removeAllSubviews;
+- (void)removeAllSubviewsOfClass:(Class)aClass;
 @property (nonatomic) CGPoint frameOrigin;
 @property (nonatomic) CGSize frameSize;
 @property (nonatomic) CGFloat frameX;
@@ -37,8 +39,20 @@
 - (void)ajustToHeightAndStopBouncing;
 @end
 
+@interface NSDate (EPCCategories)
+@property (readonly) NSString *day;
+@property (readonly) NSString *hours;
+@property (readonly) NSString *minutes;
+@property (readonly) NSString *month;
+@property (readonly) NSString *seconds;
+@property (readonly) NSString *year;
+@end
+
 @interface NSString (EPCCategories)
 - (NSString*)md5;
 - (NSURL*)urlSafe;
-- (NSString*)substringTruncatingToLength:(int)length tail:(NSString*)tail;
+- (NSString*)stringByTruncatingToLength:(int)length tail:(NSString*)tail;
+- (NSString *)stringByRemovingCharacterSet:(NSCharacterSet*)characterSet;
+- (NSString *)stringByRemovingNewLinesAndWhitespace;
+- (NSArray*)arrayByExplodingWithString:(NSString*)string;
 @end
--- EPCCategories.m
+++ EPCCategories.m
@@ -12,12 +12,27 @@
 + (id)loadFromNib {
 	return [[[NSBundle mainBundle] loadNibNamed:NSStringFromClass(self) owner:nil options:nil] lastObject];
 }
-- (UIImage*)renderToImage
+- (void)removeAllSubviews {
+	for (UIView *sub in self.subviews) {
+		[sub removeFromSuperview];
+	}
+}
+- (void)removeAllSubviewsOfClass:(Class)aClass {
+	for (UIView *sub in self.subviews) {
+		if ([sub isKindOfClass:aClass]) {
+			[sub removeFromSuperview];
+		}
+	}
+}
+- (UIImage*)renderToImageOfSize:(CGSize)size opaque:(BOOL)opaque
 {
-	UIGraphicsBeginImageContextWithOptions(self.frame.size, NO, 0.0);
+	CGRect originalRect = self.frame;
+	self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, size.width, size.height);
+	UIGraphicsBeginImageContextWithOptions(self.frame.size, opaque, 0.0);
 	[self.layer renderInContext:UIGraphicsGetCurrentContext()];
 	UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
 	UIGraphicsEndImageContext();
+	self.frame = originalRect;
 	return image;
 }
 - (CGPoint)frameOrigin {
@@ -97,6 +112,26 @@
 @end
 
 @implementation NSString (EPCCategories)
+- (NSArray*)arrayByExplodingWithString:(NSString*)string {
+	NSMutableArray *strings = [NSMutableArray array];
+	NSRange range = [self rangeOfString:string];
+	while (range.location != NSNotFound && range.location > 0) {
+		NSRange nextRange = [self rangeOfString:string options:NSLiteralSearch range:NSMakeRange(range.location+range.length, [self length] - (range.location+range.length))];
+		NSString *subs = nil;
+		if (nextRange.location == NSNotFound) {
+			subs = [self substringFromIndex:range.location+range.length];
+		}
+		else {
+			subs = [self substringWithRange:NSMakeRange(range.location+range.length, nextRange.location-(range.location+range.length))];
+		}
+		if (subs)
+			[strings addObject:subs];
+		range = nextRange;
+	}
+	if ([strings count] > 0)
+		return strings;
+	return nil;
+}
 - (NSString *) md5
 {
     const char *cStr = [self UTF8String];
@@ -116,7 +151,7 @@
 		url = [NSURL URLWithString:[self stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
 	return url;
 }
-- (NSString*)substringTruncatingToLength:(int)limit tail:(NSString*)tail {
+- (NSString*)stringByTruncatingToLength:(int)limit tail:(NSString*)tail {
 	NSString *text = self;
 	if ([text length] > limit) {
 		if ([[text substringWithRange:NSMakeRange(limit, 1)] isEqualToString:@" "]) {
@@ -128,9 +163,45 @@
 			NSRange range = [text rangeOfString:@" " options:NSBackwardsSearch range:NSMakeRange(0, limit)];
 			text = [text substringToIndex:range.location];
 		}
-		text = [text stringByAppendingString:tail];
+		if (tail)
+			text = [text stringByAppendingString:tail];
 	}
 	return text;
+}
+
+-(NSString *)stringByRemovingCharacterSet:(NSCharacterSet*)characterSet {
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	NSScanner *scanner = [[NSScanner alloc] initWithString:self];
+	[scanner setCharactersToBeSkipped:nil];
+	NSMutableString *result = [[NSMutableString alloc] init];
+	NSString *temp;
+	while (![scanner isAtEnd]) {
+		temp = nil;
+		[scanner scanUpToCharactersFromSet:characterSet intoString:&temp];
+		if (temp) [result appendString:temp];
+		if ([scanner scanCharactersFromSet:characterSet intoString:NULL]) {
+			if (result.length > 0 && ![scanner isAtEnd])
+				[result appendString:@" "];
+		}
+	}
+	[scanner release];
+	NSString *retString = [[NSString stringWithString:result] retain];
+	[result release];
+	[pool drain];
+	return [retString autorelease];
+}
+
+-(NSString *)stringByRemovingNewLinesAndWhitespace {
+	
+	// Strange New lines:
+	//  Next Line, U+0085
+	//  Form Feed, U+000C
+	//  Line Separator, U+2028
+	//  Paragraph Separator, U+2029
+	
+	NSCharacterSet *newLineAndWhitespaceCharacters = [NSCharacterSet characterSetWithCharactersInString:
+													  [NSString stringWithFormat:@" \t\n\r%d%d%d%d", 0x0085, 0x000C, 0x2028, 0x2029]];
+	return [self stringByRemovingCharacterSet:newLineAndWhitespaceCharacters];
 }
 @end
 
@@ -165,5 +236,27 @@
 		dir = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0] copy];
 	}
 	return dir;
+}
+@end
+
+@implementation NSDate (EPCCategories)
+// YYYY-MM-DD HH:MM:SS ±HHMM
+- (NSString*)day {
+	return [[self description] substringWithRange:NSMakeRange(8, 2)];
+}
+- (NSString*)hours {
+	return [[self description] substringWithRange:NSMakeRange(11, 2)];
+}
+- (NSString*)minutes {
+	return [[self description] substringWithRange:NSMakeRange(14, 2)];
+}
+- (NSString*)month {
+	return [[self description] substringWithRange:NSMakeRange(5, 2)];
+}
+- (NSString*)seconds {
+	return [[self description] substringWithRange:NSMakeRange(17, 2)];
+}
+- (NSString*)year {
+	return [[self description] substringWithRange:NSMakeRange(0, 4)];
 }
 @end
