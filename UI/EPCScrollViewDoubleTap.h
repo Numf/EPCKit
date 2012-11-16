f8c5f9ee77bee564a7470c90a1677191dd0d2ca9
Everton Cunha <ever.cunha@gmail.com>
2012-11-16 14:09:46 -0200

    comments

--- UI/EPCScrollViewDoubleTap.h
+++ UI/EPCScrollViewDoubleTap.h
@@ -1,9 +1,7 @@
 //
 //  EPCScrollViewDoubleTap.h
-//  Renner
 //
 //  Created by Everton Cunha on 15/10/12.
-//  Copyright (c) 2012 Ring. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- UI/EPCScrollViewDoubleTap.m
+++ UI/EPCScrollViewDoubleTap.m
@@ -1,9 +1,7 @@
 //
 //  EPCScrollViewDoubleTap.m
-//  Renner
 //
 //  Created by Everton Cunha on 15/10/12.
-//  Copyright (c) 2012 Ring. All rights reserved.
 //
 
 #import "EPCScrollViewDoubleTap.h"
