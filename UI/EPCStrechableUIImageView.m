2e7fb212c1fbd5bff351a882eb884d88a43ba790
Everton Cunha <ever.cunha@gmail.com>
2012-11-16 14:10:04 -0200

    fix

--- UI/EPCStrechableUIImageView.m
+++ UI/EPCStrechableUIImageView.m
@@ -19,7 +19,7 @@
 - (void)setImage:(UIImage*)img{
 	[self setContentMode:UIViewContentModeScaleToFill];
 	int w = img.size.width/2;
-	int h = img.size.height;
+	int h = img.size.height/2;
 	[super setImage:[img stretchableImageWithLeftCapWidth:w topCapHeight:h]];
 }
 
