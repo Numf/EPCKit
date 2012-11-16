0aa07323b8f3f21325d9773dd6dea9d16662f218
Everton Cunha <ever.cunha@gmail.com>
2012-11-16 14:07:31 -0200

    new methods and improvments.

--- Requests/EPCHTTPRequest.m
+++ Requests/EPCHTTPRequest.m
@@ -13,6 +13,10 @@
 @end
 
 @implementation EPCHTTPRequest
++ (EPCHTTPRequest*)requestWithRequest:(NSURLRequest *)urlRequest delegate:(id<EPCHTTPRequestDelegate>)delegate {
+	EPCHTTPRequest *me = [[[self new] initWithURLRequest:urlRequest delegate:delegate] autorelease];
+	return me;
+}
 + (EPCHTTPRequest*)requestWithURL:(NSURL *)url delegate:(id<EPCHTTPRequestDelegate>)delegate {
 	EPCHTTPRequest *me = [[[self new] initWithURL:url delegate:delegate] autorelease];
 	return me;
@@ -30,6 +34,14 @@
 	}
 	return self;
 }
+- (id)initWithURLRequest:(NSURLRequest *)urlRequest delegate:(id<EPCHTTPRequestDelegate>)delegate {
+	self = [[self class] new];
+	if (self) {
+		self.urlRequest = urlRequest;
+		self.delegate = delegate;
+	}
+	return self;
+}
 - (void)dealloc
 {
     [_operationQueue cancelAllOperations];
@@ -38,6 +50,7 @@
 	self.url = nil;
 	self.delegate = nil;
 	self.error = nil;
+	self.urlRequest = nil;
 	[_responseData release];
 	[_responseString release];
 	_responseString = nil;
@@ -51,13 +64,14 @@
 - (void)startAsynchronous {
 	NSAssert(_url, @"%@ %s no URL to start the request.", NSStringFromClass([self class]), __PRETTY_FUNCTION__);
 	
-	if (_url) {
+	if (_url || _urlRequest) {
 		[_operationQueue cancelAllOperations];
 		if (_operationQueue)
 			_operationQueue = [NSOperationQueue new];
 		
 		EPCHTTPRequest *operation = [[EPCHTTPRequest new] autorelease];
 		operation.url = _url;
+		operation.urlRequest = _urlRequest;
 		operation.delegate = (id<EPCHTTPRequestDelegate>)self;
 		[_operationQueue addOperation:operation];
 	}
@@ -99,6 +113,28 @@
 				break;
 			NSError *error = nil;
 			_responseData = [[NSData alloc] initWithContentsOfURL:_url options:NSDataReadingUncached error:&error];
+			self.error = error;
+			if ([self isCancelled])
+				break;
+			if (_responseData && !_error) {
+				if ([_delegate respondsToSelector:@selector(epcHTTPRequestFinished:)])
+					[(EPCHTTPRequest*)_delegate performSelectorOnMainThread:@selector(epcHTTPRequestFinished:) withObject:self waitUntilDone:YES];
+			}
+			else {
+				if ([_delegate respondsToSelector:@selector(epcHTTPRequestFailed:)])
+					[(EPCHTTPRequest*)_delegate performSelectorOnMainThread:@selector(epcHTTPRequestFailed:) withObject:self waitUntilDone:YES];
+			}
+			isDone = YES;
+		}
+	}
+	else if (_urlRequest) {
+		while (![self isCancelled] && !isDone) {
+			if ([_delegate respondsToSelector:@selector(epcHTTPRequestStarted:)])
+				[(EPCHTTPRequest*)_delegate performSelectorOnMainThread:@selector(epcHTTPRequestStarted:) withObject:self waitUntilDone:YES];
+			if ([self isCancelled])
+				break;
+			NSError *error = nil;
+			_responseData = [[NSURLConnection sendSynchronousRequest:_urlRequest returningResponse:nil error:&error] retain];
 			self.error = error;
 			if ([self isCancelled])
 				break;
