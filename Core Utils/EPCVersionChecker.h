7a0974ea8a3b3778d26094b3dd7636d7609ead45
Everton Cunha <ever.cunha@gmail.com>
2012-12-18 13:59:14 -0200

    new method

--- Core Utils/EPCVersionChecker.h
+++ Core Utils/EPCVersionChecker.h
@@ -12,4 +12,5 @@
 
 @interface EPCVersionChecker : NSObject
 
++ (BOOL)appChangedVersion;
 @end
--- Core Utils/EPCVersionChecker.m
+++ Core Utils/EPCVersionChecker.m
@@ -15,7 +15,7 @@ static id myself = nil;
 + (void)load {
 	myself = [[[self class] alloc] init];
 	if (myself) {
-		[[NSNotificationCenter defaultCenter] addObserver:myself selector:@selector(appDidBecomeActive) name:UIApplicationDidBecomeActiveNotification object:nil];
+		[[NSNotificationCenter defaultCenter] addObserver:myself selector:@selector(appDidBecomeActive) name:UIApplicationDidFinishLaunchingNotification object:nil];
 	}
 }
 
@@ -26,16 +26,25 @@ static id myself = nil;
 		// version changed
 		
 		[[NSUserDefaults standardUserDefaults] setObject:currentVersion forKey:kLastVersionKey];
-		[[NSUserDefaults standardUserDefaults] synchronize];
 		
 		id appdelegate = [[UIApplication sharedApplication] delegate];
 		if ([appdelegate conformsToProtocol:@protocol(EPCVersionCheckerDelegate)]) {
 			[appdelegate appicationBundleChangedFromVersion:lastVersion toVersion:currentVersion];
 		}
+		
+		// do it again in case the stanstardUserDefaults got cleared
+		[[NSUserDefaults standardUserDefaults] setObject:currentVersion forKey:kLastVersionKey];
+		[[NSUserDefaults standardUserDefaults] synchronize];
 	}
 	[[NSNotificationCenter defaultCenter] removeObserver:myself name:UIApplicationDidBecomeActiveNotification object:nil];
 	[myself release];
 	myself = nil;
+}
+
++ (BOOL)appChangedVersion {
+	NSString *currentVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString*)kCFBundleVersionKey];
+	NSString *lastVersion = [[NSUserDefaults standardUserDefaults] objectForKey:kLastVersionKey];
+	return ![currentVersion isEqualToString:lastVersion] && lastVersion != nil;
 }
 
 @end
