07583c7f3ea68d86ec108293d324d2756fa2e55d
evertoncunha <Everton@iMac-de-Everton.local>
2013-05-14 12:50:24 -0300

    wrapper for Twitter and Facebook iOS 5+

--- Twitter/EPCSocial.h
+++ Twitter/EPCSocial.h
@@ -1,24 +1,38 @@
 //
-//  EPCTwitter.h
+//  EPCSocial.h
 //
 //  Created by Everton Cunha on 13/09/12.
 //
+//  For iOS 5 or later
 
 #import <Foundation/Foundation.h>
-#import <Twitter/Twitter.h>
-#import <Accounts/Accounts.h>
 
-@class EPCTwitter;
+typedef void (^EPCSocialHandler)(BOOL success, NSError *error, id data);
 
-@protocol EPCTwitterDelegate <NSObject>
-- (void)epcTwitterAccounts:(NSArray*)accounts;
-- (void)epcTwitterAccountsDeniedWithError:(NSError*)error;
-@end
+@class EPCSocial;
 
-@interface EPCTwitter : NSObject
+@interface EPCSocial : NSObject
 
-+ (BOOL)canSendTweet;
+#pragma mark - Twitter
 
-+ (void)twitterAccountsFor:(NSObject<EPCTwitterDelegate>*)delegate;
++ (BOOL)canAccessTwitter;
+
++ (void)requestAccessToTwitter:(EPCSocialHandler)handler;
+
++ (void)requestTwitterAccountUsername:(EPCSocialHandler)handler;
+
++ (void)shareTwitterText:(NSString *)text image:(UIImage *)image url:(NSURL*)url viewController:(UIViewController*)viewController completionHandler:(EPCSocialHandler)handler;
+
+#pragma mark - Facebook
+
++ (BOOL)canAccessFacebook;
+
++ (void)requestAccessToFacebook:(EPCSocialHandler)handler;
+
++ (void)requestFacebookAccountUsername:(EPCSocialHandler)handler;
+
++ (void)shareFacebookText:(NSString *)text image:(UIImage *)image url:(NSURL*)url viewController:(UIViewController*)viewController completionHandler:(EPCSocialHandler)handler;
+
++ (void)logoutFromFacebook:(EPCSocialHandler)handler; // return YES for iOS 6 or later, but does nothing
 
 @end
--- Twitter/EPCSocial.m
+++ Twitter/EPCSocial.m
@@ -1,43 +1,259 @@
 //
-//  EPCTwitter.m
+//  EPCSocial.m
 //
 //  Created by Everton Cunha on 13/09/12.
 //
 
-#import "EPCTwitter.h"
+#import "EPCSocial.h"
+#import <Accounts/Accounts.h>
+#import <Twitter/Twitter.h>
+#import <FacebookSDK/FacebookSDK.h>
 
-@implementation EPCTwitter
+@implementation EPCSocial
 
-+ (BOOL)canSendTweet {
-	return [TWTweetComposeViewController canSendTweet];
+#pragma mark - Avaliability
+
++ (BOOL)canAccessTwitter {
+	if (IOS_VERSION_LESS_THAN(@"6.0")) {
+		return [TWTweetComposeViewController canSendTweet];
+	}
+	return [SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter];
 }
 
-+ (void)twitterAccountsFor:(NSObject<EPCTwitterDelegate>*)delegate {
-	
-	[delegate retain];
-	
-	// Create an account store object.
++ (BOOL)canAccessFacebook {
+	if (IOS_VERSION_LESS_THAN(@"6.0")) {
+		return [[FBSession activeSession] isOpen];
+	}
+	return [SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook];
+}
+
+#pragma mark - Username
+
++ (void)requestUsernameFromIdentifier:(NSString * const)identifier handler:(EPCSocialHandler)handler {
+	NSString *username = nil;
 	ACAccountStore *accountStore = [[ACAccountStore alloc] init];
-	
-	// Create an account type that ensures Twitter accounts are retrieved.
-    ACAccountType *accountType = [accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
-	
-	[accountStore requestAccessToAccountsWithType:accountType withCompletionHandler:^(BOOL granted, NSError *error) {
-		if ([delegate retainCount] > 1) {
-			if(granted) {
-				// Get the list of Twitter accounts.
-				NSArray *accountsArray = [accountStore accountsWithAccountType:accountType];
-				[delegate performSelectorOnMainThread:@selector(epcTwitterAccounts:) withObject:accountsArray waitUntilDone:YES];
-			}
-			else {
-				DLog(@"%s Error: %@", __PRETTY_FUNCTION__, [error description]);
-				[delegate performSelectorOnMainThread:@selector(epcTwitterAccountsDeniedWithError:) withObject:error waitUntilDone:YES];
-			}
-		}
-		
-		[delegate release];
-		[accountStore release];
+	ACAccountType *accountType = [accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+	NSArray *accountsArray = [accountStore accountsWithAccountType:accountType];
+	ACAccount *account = [accountsArray firstObject];
+	username = account.username;
+	handler(accountType.accessGranted,nil,username);
+}
+
++ (void)requestTwitterAccountUsername:(EPCSocialHandler)handler {
+	[self requestUsernameFromIdentifier:ACAccountTypeIdentifierTwitter handler:handler];
+}
+
++ (void)RequestFacebookUsernameOnIOS5:(EPCSocialHandler)handler {
+	[FBRequestConnection startForMeWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+		NSString *userName = [result isKindOfClass:[NSDictionary class]]?[result objectForKey:@"name"]:nil;
+		handler(error!=nil, error, userName);
 	}];
 }
 
++ (void)requestFacebookAccountUsername:(EPCSocialHandler)handler {
+	if (IOS_VERSION_LESS_THAN(@"6.0")) {
+		if ([[FBSession activeSession] isOpen]) {
+			[self RequestFacebookUsernameOnIOS5:handler];
+		}
+		else {
+			[self requestAccessToFacebook:^(BOOL hasAccess, NSError *error, id data) {
+				if (hasAccess) {
+					[self RequestFacebookUsernameOnIOS5:handler];
+				}
+				else {
+					handler(hasAccess, error, nil);
+				}
+			}];
+		}
+	}
+	else {
+		[self requestUsernameFromIdentifier:ACAccountTypeIdentifierFacebook handler:handler];
+	}
+}
+
+#pragma mark - Access
+
++ (void)requestAccessToTwitter:(EPCSocialHandler)handler {
+	[self requestAccessToAccountsWithIdentifier:ACAccountTypeIdentifierTwitter handler:^(BOOL success, NSError *error, id data) {
+		handler(success, nil, nil);
+	}];
+}
+
++ (void)requestAccessToFacebook:(EPCSocialHandler)handler {
+	if (IOS_VERSION_LESS_THAN(@"6.0")) {
+		NSArray *permissions = [self facebookPermissions];
+		BOOL publish = NO;
+		for (NSString *per in permissions) {
+			if ([per rangeOfString:@"publish"].location != NSNotFound) {
+				publish = YES;
+				break;
+			}
+		}
+		if (publish) {
+			[FBSession openActiveSessionWithPublishPermissions:permissions defaultAudience:FBSessionDefaultAudienceEveryone allowLoginUI:<#(BOOL)#> completionHandler:<#^(FBSession *session, FBSessionState status, NSError *error)handler#>]
+		}
+		else {
+			[FBSession openActiveSessionWithReadPermissions:permissions allowLoginUI:YES completionHandler:^(FBSession *session, FBSessionState status, NSError *error) {
+				handler(status == FBSessionStateOpen, error, nil);
+			}];
+		}
+	}
+	else {
+		[self requestAccessToAccountsWithIdentifier:ACAccountTypeIdentifierFacebook handler:^(BOOL success, NSError *error, id data) {
+			handler(success, nil, nil);
+		}];
+	}
+}
+
++ (void)requestAccessToAccountsWithIdentifier:(NSString * const)identifier handler:(EPCSocialHandler)handler {
+	
+	ACAccountStore *accountStore = [[ACAccountStore alloc] init];
+	
+    ACAccountType *accountType = [accountStore accountTypeWithAccountTypeIdentifier:identifier];
+	
+	if (IOS_VERSION_LESS_THAN(@"6.0")) {
+		[accountStore requestAccessToAccountsWithType:accountType withCompletionHandler:^(BOOL granted, NSError *error) {
+			handler(granted, error, accountStore);
+			
+			[accountStore release];
+		}];
+	}
+	else {
+		
+		NSDictionary *options = nil;
+		
+		if (identifier == ACAccountTypeIdentifierFacebook) {
+			options = [self facebookOptions];
+		}
+		
+		[accountStore requestAccessToAccountsWithType:accountType options:options completion:^(BOOL granted, NSError *error) {
+			handler(granted, error, accountStore);
+			
+			[accountStore release];
+		}];
+	}
+}
+
++ (void)logoutFromFacebook:(EPCSocialHandler)handler {
+	if (IOS_VERSION_LESS_THAN(@"6.0")) {
+		[[FBSession activeSession] close];
+		if (handler!=nil) {
+			handler(YES, nil, nil);
+		}
+	}
+	else {
+		if (handler!=nil) {
+			handler(YES, nil, nil);
+		}
+	}
+}
+
++ (NSArray*)facebookPermissions {
+	NSArray *permissions = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"FacebookPermissionsKey"];
+	NSAssert(permissions != nil && [permissions isKindOfClass:[NSArray class]], @"FacebookPermissionsKey is not set or isn't an array in Info.plist");
+	return permissions;
+}
+
++ (int)facebookAudienceForIOS5 {
+#warning  AUQI!
+	
+	FBSessionDefaultAudienceEveryone
+}
+
++ (NSString* const)facebookAudience {
+	NSString *audience = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"FacebookAudienceKey"];
+	if ([audience isEqualToString:@"ACFacebookAudienceEveryone"]) {
+		audience = ACFacebookAudienceEveryone;
+	}
+	else if ([audience isEqualToString:@"ACFacebookAudienceFriends"]) {
+		audience = ACFacebookAudienceFriends;
+	}
+	else if ([audience isEqualToString:@"ACFacebookAudienceOnlyMe"]) {
+		audience = ACFacebookAudienceOnlyMe;
+	}
+	NSAssert(audience != nil, @"FacebookAudienceKey is not set in Info.plist");
+}
+
++ (NSDictionary*)facebookOptions {
+	NSString *facebookAppId = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"FacebookAppID"];
+	NSAssert(facebookAppId != nil, @"FacebookAppID is not set in Info.plist");
+	
+	NSString *const audience = [self facebookAudience];
+	
+	NSArray *permissions = [self facebookPermissions];
+	
+	NSDictionary *options =  @{ACFacebookAppIdKey: facebookAppId, ACFacebookPermissionsKey: permissions, ACFacebookAudienceKey: audience};
+	
+	return options;
+}
+
+#pragma mark - Share Content
+
++ (void)shareForServiceType:(NSString *)serviceType text:(NSString *)text image:(UIImage *)image url:(NSURL *)url viewController:(UIViewController *)viewController completionHandler:(EPCSocialHandler)handler {
+	SLComposeViewController *mySLComposerSheet = [SLComposeViewController composeViewControllerForServiceType:serviceType];
+	
+	if (text) {
+		[mySLComposerSheet setInitialText:text];
+	}
+	if (image) {
+		[mySLComposerSheet addImage:image];
+	}
+	if (url) {
+		[mySLComposerSheet addURL:url];
+	}
+	
+	[mySLComposerSheet setCompletionHandler:^(SLComposeViewControllerResult result) {
+		
+		switch (result) {
+			case SLComposeViewControllerResultCancelled:
+				DLog(@"Post Canceled");
+				[mySLComposerSheet dismissViewControllerAnimated:YES completion:nil];
+				break;
+			case SLComposeViewControllerResultDone:
+				DLog(@"Post Sucessful");
+				break;
+			default:
+				break;
+		}
+		
+		if (handler != nil) {
+			handler(YES, nil, nil);
+		}
+	}];
+	
+	[viewController presentViewController:mySLComposerSheet animated:YES completion:nil];
+}
+
++ (void)shareFacebookText:(NSString *)text image:(UIImage *)image url:(NSURL *)url viewController:(UIViewController *)viewController completionHandler:(EPCSocialHandler)handler {
+	if (IOS_VERSION_LESS_THAN(@"6.0")) {
+		//TODO: ios 5 aqui
+	}
+	else {
+		[self shareForServiceType:SLServiceTypeFacebook text:text image:image url:url viewController:viewController completionHandler:handler];
+	}
+}
+
++ (void)shareTwitterText:(NSString *)text image:(UIImage *)image url:(NSURL *)url viewController:(UIViewController *)viewController completionHandler:(EPCSocialHandler)handler {
+	if (IOS_VERSION_LESS_THAN(@"6.0")) {
+		TWTweetComposeViewController *tweet = [[TWTweetComposeViewController alloc] initWithNibName:nil bundle:nil];
+		if (url) {
+			[tweet addURL:url];
+		}
+		if (image) {
+			[tweet addImage:image];
+		}
+		if (text) {
+			[tweet setInitialText:text];
+		}
+		[viewController presentViewController:tweet animated:YES completion:nil];
+		[tweet release];
+		
+		if (handler != nil) {
+			handler(YES, nil, nil);
+		}
+	}
+	else {
+		[self shareForServiceType:SLServiceTypeTwitter text:text image:image url:url viewController:viewController completionHandler:handler];
+	}
+}
 @end
