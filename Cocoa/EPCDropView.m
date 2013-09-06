2042d645e0e44af7d3f56eafe133559711ba9713
Everton <evertonmaverick@MacBook-de-Everton.local>
2013-09-06 18:02:33 -0300

    drop mac view

--- Cocoa/EPCDropView.m
+++ Cocoa/EPCDropView.m
@@ -10,6 +10,18 @@
 
 @implementation EPCDropView
 
+@synthesize delegate = _delegate;
+@synthesize supportedFileExtensions = _supportedFileExtensions;
+@synthesize allowFolders = _allowFolders;
+
+- (void)dealloc
+{
+	self.delegate = nil;
+    self.supportedFileExtensions = nil;
+	[self clearFileArray];
+	[super dealloc];
+}
+
 - (void)awakeFromNib {
 	[super awakeFromNib];
 	
@@ -18,28 +30,42 @@
 }
 
 - (NSDragOperation)draggingEntered:(id <NSDraggingInfo>)sender {
-    if ((NSDragOperationGeneric & [sender draggingSourceOperationMask])
-		== NSDragOperationGeneric) {
-        return NSDragOperationCopy;
-    }
-    else {
-        return NSDragOperationNone;
-    }
+	[self checkFilesFromSender:sender];
+	
+	if (_allowing) {
+		if ((NSDragOperationGeneric & [sender draggingSourceOperationMask])
+			== NSDragOperationGeneric) {
+			if ([self.delegate respondsToSelector:@selector(dropView:dragEnteredHoldingAlt:)]) {
+				[self.delegate dropView:self dragEnteredHoldingAlt:NO];
+			}
+			return NSDragOperationCopy;
+		}
+		else if ((NSDragOperationCopy & [sender draggingSourceOperationMask]) == NSDragOperationCopy) {
+			if ([self.delegate respondsToSelector:@selector(dropView:dragEnteredHoldingAlt:)]) {
+				[self.delegate dropView:self dragEnteredHoldingAlt:YES];
+			}
+			return NSDragOperationCopy;
+		}
+	}
+    
+	return NSDragOperationNone;
 }
 
 - (NSDragOperation)draggingUpdated:(id <NSDraggingInfo>)sender {
-	_holdingAlt = NO;
-    if ((NSDragOperationGeneric & [sender draggingSourceOperationMask])
-		== NSDragOperationGeneric) {
-        return NSDragOperationCopy;
-    }
-	else if ((NSDragOperationCopy & [sender draggingSourceOperationMask]) == NSDragOperationCopy) {
-		_holdingAlt = YES;
-		return NSDragOperationCopy;
+	
+	if (_allowing) {
+		if ((NSDragOperationGeneric & [sender draggingSourceOperationMask])
+			== NSDragOperationGeneric) {
+			_holdingAlt = NO;
+			return NSDragOperationCopy;
+		}
+		else if ((NSDragOperationCopy & [sender draggingSourceOperationMask]) == NSDragOperationCopy) {
+			_holdingAlt = YES;
+			return NSDragOperationCopy;
+		}
 	}
-    else {
-        return NSDragOperationNone;
-    }
+	
+	return NSDragOperationNone;
 }
 
 - (BOOL)prepareForDragOperation:(id <NSDraggingInfo>)sender {
@@ -47,31 +73,83 @@
 }
 
 - (BOOL)performDragOperation:(id <NSDraggingInfo>)sender {
-    NSPasteboard *paste = [sender draggingPasteboard];
-	//gets the dragging-specific pasteboard from the sender
-    NSArray *types = [NSArray arrayWithObjects:
-					  NSFilenamesPboardType, nil];
-	//a list of types that we can accept
-    NSString *desiredType = [paste availableTypeFromArray:types];
-    NSData *carriedData = [paste dataForType:desiredType];
-	
-    if (nil == carriedData) {
-        //the operation failed for some reason
-		DLog(@"Paste Error - Sorry, but the past operation failed");
-        return NO;
-    }
-    else {
-		NSArray *fileArray = [paste propertyListForType:NSFilenamesPboardType];
-		return [self.delegate dropView:self shouldAcceptFiles:fileArray holdingAlt:_holdingAlt];
-    }
-	return NO;
+	return _allowing;
 }
 
 - (void)concludeDragOperation:(id <NSDraggingInfo>)sender
 {
-	NSPasteboard *paste = [sender draggingPasteboard];
-	NSArray *fileArray = [paste propertyListForType:NSFilenamesPboardType];
-	[self.delegate dropView:self didReceiveFiles:fileArray holdingAlt:_holdingAlt];
+	if ([self.delegate respondsToSelector:@selector(dropView:didReceiveFiles:holdingAlt:)]) {
+		[self.delegate dropView:self didReceiveFiles:_fileArray holdingAlt:_holdingAlt];
+	}
+	if ([self.delegate respondsToSelector:@selector(dropView:dragEndedAccepting:)]) {
+		[self.delegate dropView:self dragEndedAccepting:YES];
+	}
+	[self clearFileArray];
 }
 
+- (void)draggingExited:(id<NSDraggingInfo>)sender {
+	[self clearFileArray];
+	if ([self.delegate respondsToSelector:@selector(dropView:dragEndedAccepting:)]) {
+		[self.delegate dropView:self dragEndedAccepting:NO];
+	}
+}
+
+- (void)clearFileArray {
+	[_fileArray release];
+	_fileArray = nil;
+}
+
+- (void)checkFilesFromSender:(id <NSDraggingInfo>)sender {
+	[self clearFileArray];
+	if (!_fileArray) {
+		NSPasteboard *paste = [sender draggingPasteboard];
+		_fileArray = [[paste propertyListForType:NSFilenamesPboardType] retain];
+		_allowing = [self allowsFiles:_fileArray];
+	}
+}
+
+- (BOOL)allowsFiles:(NSArray*)files {
+	NSFileManager *fm = [NSFileManager defaultManager];
+	for (NSString* path in files) {
+		NSString *ext = [path pathExtension];
+		if ([ext length] == 0) {
+			BOOL *isDir = NO;
+			if([fm fileExistsAtPath:path isDirectory:isDir]) {
+				if (isDir) {
+					return self.allowFolders;
+				}
+			}
+		}
+		else {
+			if ([self.supportedFileExtensions containsObject:ext]) {
+				return YES;
+			}
+		}
+	}
+	return NO;
+}
+
+- (NSArray*)filteredFilesFromArray:(NSArray*)array {
+	NSMutableArray *result = [NSMutableArray arrayWithCapacity:[array count]];
+	NSFileManager *fm = [NSFileManager defaultManager];
+	for (NSString* path in array) {
+		NSString *ext = [path pathExtension];
+		if ([ext length] == 0) {
+			BOOL *isDir = NO;
+			if([fm fileExistsAtPath:path isDirectory:isDir]) {
+				if (isDir) {
+					if (self.allowFolders) {
+						[result addObject:path];
+					}
+				}
+			}
+		}
+		else {
+			if ([self.supportedFileExtensions containsObject:ext]) {
+				[result addObject:path];
+			}
+		}
+	}
+	return result;
+}
 @end
